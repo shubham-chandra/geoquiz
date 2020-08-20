4269ce07eb1c6c1c8e8e24cdbef804820a5e7630
shubham chandra <shubhamchandra317@gmail.com>
2020-08-20 17:58:59 +0530

    Add Landscape layout file

--- app/src/main/java/com/shubham/geoquiz/MainActivity.kt
+++ app/src/main/java/com/shubham/geoquiz/MainActivity.kt
@@ -2,13 +2,14 @@ package com.shubham.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-
+private const  val  TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton:Button
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private  lateinit var questionTextView: TextView
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
+
 
     private val questionBank = listOf(
         Question(R.string.question_oceans,true),
@@ -32,6 +34,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"OnCreate called")
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -87,5 +91,30 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this,msgTextId,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"OnStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"OnResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"OnPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"OnStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    Log.d(TAG,"OnDestroy called")
     }
 }
